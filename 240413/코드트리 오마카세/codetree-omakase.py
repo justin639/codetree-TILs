cbdc16cc728d042e68b00983fe59e946e1117518
Junseok Kim <75320238+justin639@users.noreply.github.com>
2024-04-13 13:23:27 +0900

    Title: 코드트리 오마카세, Time: 6551ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

---
+++ 240413/코드트리 오마카세/codetree-omakase.py
@@ -0,0 +1,101 @@
+from collections import deque
+
+SUSHI = 0
+CUSTOMER = 1
+PICTURE = 2
+
+L, Q = map(int, input().split())
+que_l = [[0]*15000 for _ in range(L)]
+# print(que_l)
+que = deque(que_l)
+name_list = []
+table_list = [[0]*15000 for _ in range(L)]
+customer_list = [-1] * 15000
+T = 1
+sushi_T = 0
+custom_T = 0
+
+def name_hash(name):
+    for i in range(len(name_list)):
+        if name_list[i] == name:
+            return i
+
+    name_list.append(name)
+    return len(name_list)-1
+
+for _ in range(Q):
+    line = list(input().split())
+    in_type, t, x, name, n = -1, 0, 0, "", 0
+    if len(line) == 4:
+        in_type, t, x, name = line
+        in_type = SUSHI
+    elif len(line) == 5:
+        in_type, t, x, name, n = line
+        in_type = CUSTOMER
+    else:
+        in_type, t = line
+        in_type = PICTURE
+
+    t = int(t)
+    x = int(x)
+    n = int(n)
+    while T < t:
+        last = que.pop()
+        que.appendleft(last)
+        que_l = list(que)
+        for j in range(len(name_list)):
+            if customer_list[j] != -1:
+                i = customer_list[j]
+                if table_list[i][j] > 0 and que_l[i][j] > 0:
+                    left = table_list[i][j] - que_l[i][j]
+                    if left > 0:
+                        table_list[i][j] = left
+                        sushi_T -= que_l[i][j]
+                        que_l[i][j] = 0
+                    elif left == 0:
+                        custom_T -= 1
+                        sushi_T -= table_list[i][j]
+                        customer_list[j] = -1
+                        table_list[i][j] = 0
+                        que_l[i][j] = 0
+                    else:
+                        custom_T -= 1
+                        sushi_T -= table_list[i][j]
+                        customer_list[j] = -1
+                        table_list[i][j] = 0
+                        que_l[i][j] = -left
+                
+        que = deque(que_l)
+        T += 1
+
+    que_l = list(que)
+    if in_type == 0:
+        name_n = name_hash(name)
+        if table_list[x][name_n] > 0:
+            table_list[x][name_n] -= 1
+            if table_list[x][name_n] == 0:
+                custom_T -= 1
+        else:
+            que_l[x][name_n] += 1
+            sushi_T += 1
+            que = deque(que_l)
+    elif in_type == 1:
+        name_n = name_hash(name)
+        left = n - que_l[x][name_n]
+        if left > 0:
+            table_list[x][name_n] = left
+            customer_list[name_n] = x
+            custom_T += 1
+            sushi_T -= que_l[x][name_n]
+            que_l[x][name_n] = 0
+        elif left == 0:
+            table_list[x][name_n] = 0
+            sushi_T -= n
+            que_l[x][name_n] = 0
+        else:
+            table_list[x][name_n] = 0
+            que_l[x][name_n] = -left
+            sushi_T -= n
+        que = deque(que_l)
+    elif in_type == 2:
+        print(str(custom_T) + " " + str(sushi_T))
